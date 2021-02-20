935fc5eaa4f75b0d0ea3162071a4d35a0296a5a0
thallesyasmim <thallesgabriel1307@gmail.com>
2021-02-20 16:12:23 -0300

    get page content

--- server.js
+++ server.js
@@ -3,19 +3,27 @@ const puppeteer = require('puppeteer')
 
 const server = express()
 
-server.get('/', (request, response) => {
-    response.send('Hello World!')
+server.get('/', async (request, response) => {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto('https://www.alura.com.br/formacao-front-end')
+  
+    const pageContent = await page.evaluate(() => {
+        const data = {
+            width: document.documentElement.clientWidth,
+            height: document.documentElement.clientHeight,
+            title: document.querySelector('.formacao-headline-titulo').innerHTML,
+            subtitle: document.querySelector('.formacao-headline-subtitulo').innerHTML,
+            workload: document.getElementsByClassName('formacao__info-destaque')[0].innerHTML,
+            courses: document.getElementsByClassName('formacao__info-destaque')[1].innerHTML
+        }
+
+        return data
+    })
+
+    await browser.close();
+    response.json(pageContent)
 })
 
 
 server.listen(3000, () => console.log('*** Server is running ***'))
-
-async function start () {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.goto('https://www.alura.com.br/formacao-front-end')
-
-
-
-  await browser.close();
-}
