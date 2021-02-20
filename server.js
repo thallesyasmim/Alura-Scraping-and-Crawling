2204ef31ff2df1fd129bc94b75cbbd2b09f83ed6
thallesyasmim <thallesgabriel1307@gmail.com>
2021-02-20 15:53:06 -0300

    on server

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-const puppeteer = require('puppeteer');
+const express = require('express')
+const puppeteer = require('puppeteer')
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://www.alura.com.br/formacao-front-end');
+const server = express()
+
+server.get('/', (request, response) => {
+    response.send('Hello World!')
+})
 
 
-  
+server.listen(3000, () => console.log('*** Server is running ***'))
+
+async function start () {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto('https://www.alura.com.br/formacao-front-end')
+
+
+
   await browser.close();
-})();
+}
